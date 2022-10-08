bb240f4c3538574931e390e887a9d675d56a3e2b
JNaeder <39416919+JNaeder@users.noreply.github.com>
2022-10-08 11:53:23 -0400

    I dont remember what I changed, but it's better

--- bot.py
+++ bot.py
@@ -6,14 +6,18 @@ from todo_list import *
 
 # Setup Stuff
 load_dotenv()
-DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+emojis = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣', '🔟']
+
+all_lists = []
+
 test_data = Todo_List("Test_List", "Me")
 test_data.load_JSON(open("./test_data.json"))
-emojis = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣', '🔟']
+all_lists.append(test_data)
 
 
 # -----Helper functions-----
@@ -30,35 +34,97 @@ async def add_emojis(the_message, the_list):
         await the_message.add_reaction(emojis[index])
 
 
+def get_list_by_name(list_name):
+    for some_list in all_lists:
+        if some_list.get_todo_list_name() == list_name:
+            return some_list
+    return None
+
+
+def get_list_by_message_id(message_id):
+    for some_list in all_lists:
+        if some_list.get_message_id() == message_id:
+            return some_list
+    return None
+
+
+def get_user_lists(member_id):
+    output = []
+    for some_list in all_lists:
+        if some_list.is_user_member(member_id):
+            output.append(some_list)
+    return output
+
+
 # -----Commands-----
 
 # Create new list
-@bot.command(name="makelist")
-async def make_new_list(ctx: discord.ext.commands.Context, *, list_name):
-    global test_data
-    test_data = Todo_List(list_name, ctx.author)
+@bot.command(name="makelist", help="Make a new ToDo list")
+async def make_new_list(ctx: discord.ext.commands.Context, permission, *, list_name):
+    new_list = Todo_List(list_name, ctx.author.id)
+    if permission == all:
+        for member in ctx.guild.members:
+            new_list.add_member(member.id)
+    all_lists.append(new_list)
     await ctx.message.add_reaction('✅')
 
 
+# Show users lists
+@bot.command(name="mylists", help="Display ToDo lists for specific user")
+async def show_my_lists(ctx: discord.ext.commands.Context):
+    the_user = ctx.author.id
+    user_lists_names = [x.get_todo_list_name() for x in get_user_lists(the_user)]
+    if len(user_lists_names) > 0:
+        await ctx.send("\n".join(user_lists_names))
+    else:
+        await ctx.send("You have no lists!")
+
+
 # Show List Command
-@bot.command(name="showlist")
-async def show_list(ctx: discord.ext.commands.Context):
-    output = make_output_list(test_data.get_todo_list(), test_data.get_todo_list_name())
-    the_message = await ctx.send("\n".join(output))
-    await add_emojis(the_message, test_data.get_todo_list())
+@bot.command(name="showlist", help="Display a ToDo list in chat")
+async def show_list(ctx: discord.ext.commands.Context, *, list_name):
+    user_id = ctx.author.id
+    the_list = get_list_by_name(list_name)
+    if the_list is None:
+        await ctx.send("⚠No list by that name ⚠")
+        return
+    if the_list.is_user_member(user_id):
+        output = make_output_list(the_list.get_todo_list(), the_list.get_todo_list_name())
+        the_message = await ctx.send("\n".join(output))
+        the_list.set_message_id(the_message.id)
+        await add_emojis(the_message, the_list.get_todo_list())
+    else:
+        await ctx.send("⛔You are not a member of this list.⛔")
 
 
 # Show list of Completed Items
-@bot.command(name="showcomplete")
+@bot.command(name="showcomplete", help="Display all completed items ")
 async def show_complete(ctx: discord.ext.commands.Context):
     output = make_output_list(test_data.get_completed(), test_data.get_todo_list_name(), "--Completed")
     await ctx.send("\n".join(output))
 
 
 # Add item Command
-@bot.command(name="add", help="Testing")
-async def add_item(ctx: discord.ext.commands.Context, *, new_item):
-    test_data.add_item(new_item)
+@bot.command(name="add", help="Add new item to list. Need quotes around list name")
+async def add_item(ctx: discord.ext.commands.Context, list_name, *, new_item):
+    the_list = get_list_by_name(list_name)
+    if the_list is None:
+        await ctx.send("⚠No list by that name ⚠")
+        return
+    the_list.add_item(new_item)
+    await ctx.message.add_reaction('✅')
+
+
+@bot.command(name="add2", help="Add new item to list. Separate list name with |")
+async def add_item(ctx: discord.ext.commands.Context, *, the_input):
+    array = the_input.split("|")
+    list_name = array[0].strip()
+    new_item = array[1].strip()
+    the_list = get_list_by_name(list_name)
+    if the_list is None:
+        await ctx.send("⚠No list by that name ⚠")
+        return
+    the_list.add_item(new_item)
     await ctx.message.add_reaction('✅')
 
 
@@ -69,13 +135,19 @@ async def add_item(ctx: discord.ext.commands.Context, *, new_item):
 async def on_reaction_add(reaction: discord.Reaction, user: discord.Member):
     if user.id == bot.user.id:
         return
+    if reaction.emoji not in emojis:
+        return
+    the_list = get_list_by_message_id(reaction.message.id)
+    if not the_list.is_user_member(user.id):
+        return
+
     item_index = emojis.index(reaction.emoji)
-    test_data.complete_item(test_data.get_todo_list()[item_index])
-    output = make_output_list(test_data.get_todo_list(), test_data.get_todo_list_name())
+    the_list.complete_item(the_list.get_todo_list()[item_index])
+    output = make_output_list(the_list.get_todo_list(), the_list.get_todo_list_name())
     await reaction.message.edit(content="\n".join(output))
     await reaction.message.clear_reactions()
-    await add_emojis(reaction.message, test_data.get_todo_list())
+    await add_emojis(reaction.message, the_list.get_todo_list())
 
 
 # Start Bot
-bot.run(DISCORD_TOKEN)
+bot.run(TOKEN)
--- todo_list.py
+++ todo_list.py
@@ -6,10 +6,11 @@ class Todo_List:
     def __init__(self, list_name, creator):
         self.creator = creator
         self.creation_date = datetime.datetime.now()
-        self.members = []
+        self.members = [creator]
         self.completed = []
         self.todolist = []
         self.todoListName = list_name
+        self.message_id = None
 
     def load_JSON(self, json_data):
         raw_data = json.load(json_data)
@@ -29,9 +30,21 @@ class Todo_List:
     def get_completed(self):
         return self.completed
 
+    def get_message_id(self):
+        return self.message_id
+
+    def set_message_id(self, message_id):
+        self.message_id = message_id
+
     def add_item(self, new_item):
         self.todolist.append(new_item)
+
+    def add_member(self, member_id):
+        self.members.append(member_id)
 
     def complete_item(self, the_item):
         self.todolist.remove(the_item)
         self.completed.append(the_item)
+
+    def is_user_member(self, member_id):
+        return member_id in self.members
